8c04234aa5b383a87bedecd19c074140cfe587f4
romche2h <rommche98@gmail.com>
2024-06-12 13:37:56 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className='card'>
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count} fuck
+          count is {count} fuck you!
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
